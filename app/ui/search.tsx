bf2393a3e3084253f7e309e75d4ce0a8e6f36ebb
Sol <solmseda@gmail.com>
2025-02-19 12:45:25 -0300

    fix de um erro, chamando UseRouter dentro de um callback quando deveria estar fora

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,9 +7,9 @@ import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const handleSearch = useDebouncedCallback((term) => {
   const { replace } = useRouter();
 
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
